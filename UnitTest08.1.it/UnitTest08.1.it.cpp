72afb4ef8635484ea44a54f41dfa7ec1f5ee3ad6
Danylo <Danylo.Brudnyi.RI.2023@lpnu.ua>
2023-11-15 10:20:30 +0200

    second test

---
+++ UnitTest08.1.it/UnitTest08.1.it.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include "CppUnitTest.h"
+#include "../PR-08.1.iterac/PR-08.1.iterac.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest081it
+{
+	TEST_CLASS(UnitTest081it)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			{
+				char str[101] = "a**b**c**d";
+
+				int pos = findThirdCommaIndex(str);
+
+				Assert::AreEqual(pos, -1);
+			}
+		}
+	};
+}
